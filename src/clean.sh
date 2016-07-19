b9ee119b12c576c0ef61221067bbf759b20951d4
Jesse Ditson <jesse.ditson@gmail.com>
2016-07-18 22:20:25 -0700

    add clean script

---
+++ src/clean.sh
@@ -0,0 +1,14 @@
+rm -rf `cat <<EOF
+fonts/
+index.html
+jsonapi-express_examples_operations.js.html
+jsonapi-knex_index.js.html
+jsonapi-schema_index.js.html
+module-jsonapi-express_operations.html
+module-jsonapi-knex.html
+module-jsonapi-schema.html
+scripts/
+styles/
+tutorial-guide.html
+tutorial-modules.html
+EOF`
