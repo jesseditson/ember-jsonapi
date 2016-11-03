e3da201239d13784bbdd23a6fa94691264ce5949
Jesse Ditson <jesse.ditson@gmail.com>
2016-11-02 19:52:29 -0700

    add option to pass model configuration objects and/or defaults

--- addon/JSONAPIModel.js
+++ addon/JSONAPIModel.js
@@ -3,21 +3,28 @@ import attr from 'ember-data/attr';
 import relationships from 'ember-data/relationships';
 import { singularize } from 'ember-inflector';
 
-export default function JSONAPIModel(schema) {
-  return Model.extend(schemaParser(schema));
+export default function JSONAPIModel(schema, configs) {
+  return Model.extend(schemaParser(schema, configs));
 }
 
-function schemaParser(schema) {
+function parseConfig(config) {
+  if (config === undefined) return;
+  if (typeof config === 'object') return config;
+  return { defaultValue: config };
+}
+
+function schemaParser(schema, configs) {
   return Object.keys(schema).reduce((model, key) => {
     var info = schema[key];
+    var config = parseConfig(configs[key]);
     if (typeof info === 'string') {
-      model[key] = attr(info);
+      model[key] = attr(info, config);
     } else if (info === null) {
-      model[key] = attr();
+      model[key] = attr(null, config);
     } else if (info.relationship) {
-      model[key] = relationships[info.relationship](singularize(info.type));
+      model[key] = relationships[info.relationship](singularize(info.type), config);
     } else {
-      model[key] = attr(info.type);
+      model[key] = attr(info.type, config);
     }
     return model;
   }, {});
